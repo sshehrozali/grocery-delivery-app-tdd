318ff9fec6d84a98d5c47b198e681085a9ea55d5
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-25 10:11:20 +0000

    OfferRepository: Added code for making this as repository

--- src/main/java/com/ordering/app/OfferRepository.java
+++ src/main/java/com/ordering/app/OfferRepository.java
@@ -1,2 +1,8 @@
-package com.ordering.app;public interface OfferRepository {
+package com.ordering.app;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OfferRepository extends JpaRepository<Offer, Integer> {
 }
