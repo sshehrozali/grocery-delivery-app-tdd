bf3671a7e7402bfa2f8c7ede8b6ec19250887608
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-25 09:42:42 +0000

    Line: Added code for making this entity

--- src/main/java/com/ordering/app/Line.java
+++ src/main/java/com/ordering/app/Line.java
@@ -1,4 +1,22 @@
 package com.ordering.app;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class Line {
+    @Id
+    private Integer id;
+    @OneToOne
+    private Item itemId;
+    private Integer quantity;
 }
