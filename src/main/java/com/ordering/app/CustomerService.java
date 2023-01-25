f20918058fd3c6d5078ce3e924903434c62fd6ae
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-25 09:39:16 +0000

    Line: Created new Java class file CustomerService: Incomplete

--- src/main/java/com/ordering/app/CustomerService.java
+++ src/main/java/com/ordering/app/CustomerService.java
@@ -2,7 +2,14 @@ package com.ordering.app;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CustomerService {
 
+
+
+    public List<Item> getAllItems() {
+        return
+    }
 }
