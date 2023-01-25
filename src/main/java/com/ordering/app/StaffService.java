a75292d4e982780179663c3eeef24a23320eac60
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-25 16:00:10 +0000

    Refactored

--- src/main/java/com/ordering/app/StaffService.java
+++ src/main/java/com/ordering/app/StaffService.java
@@ -18,6 +18,21 @@ public class StaffService {
     }
 
     public List<Line> saveNewItemsOnlyInInventoryWithQuantity(List<Line> items) {
+        if (items.isEmpty()) {
+            throw new RuntimeException("Request can't be empty. Please add Items for saving in inventory.");
+        }
+
+        items.forEach(line -> {
+            if (line.getItemId().getItemId().isEmpty()
+                    || line.getItemId().getItemName().isEmpty()
+                    || line.getItemId().getDescription().isEmpty()
+                    || line.getItemId().getPrice().equals(null)
+                    || line.getItemId().getCost().equals(null)
+            ) {
+
+                throw new RuntimeException("Values of Items can't be empty. Please check before saving.");
+            }
+        });
         return lineRepository.saveAll(items);
     }
 }
