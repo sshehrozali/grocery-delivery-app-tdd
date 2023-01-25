99015771b28771ceec642146c76620a4408969e7
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-25 09:33:03 +0000

    shouldGetAllItems: Test failed

--- src/test/java/com/ordering/app/CustomerServiceTest.java
+++ src/test/java/com/ordering/app/CustomerServiceTest.java
@@ -1,4 +1,19 @@
 package com.ordering.app;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
 public class CustomerServiceTest {
+    private CustomerService customerService;
+
+    @Test
+    @DisplayName("Should Get All Items")
+    void shouldGetAllItems() {
+        assertThat(customerService.getAllItems()).isNotNull();
+    }
 }
