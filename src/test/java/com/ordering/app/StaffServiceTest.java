6abce18037360bd9220055937cebf8e4f59d6fb0
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-25 14:30:04 +0000

    Injected StaffService LineRepository Added @InjectMocks to StaffService Added @Mock to LineRepository

--- src/test/java/com/ordering/app/StaffServiceTest.java
+++ src/test/java/com/ordering/app/StaffServiceTest.java
@@ -1,9 +1,14 @@
 package com.ordering.app;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class StaffServiceTest {
+    @InjectMocks
     private StaffService staffService;
+    @Mock
+    private LineRepository lineRepository;
 }
