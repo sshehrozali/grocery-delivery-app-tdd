d488daa66542ea9c9c3c1ef4664f0fd8da3433f9
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-25 09:44:10 +0000

    CustomerService: Constructor Injected dependencies

--- src/main/java/com/ordering/app/CustomerService.java
+++ src/main/java/com/ordering/app/CustomerService.java
@@ -1,13 +1,15 @@
 package com.ordering.app;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CustomerService {
-
-
+    private final ItemRepository itemRepository;
+    private final LineRepository lineRepository;
 
     public List<Item> getAllItems() {
         return
