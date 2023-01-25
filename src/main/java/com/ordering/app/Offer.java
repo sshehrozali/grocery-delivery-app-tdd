e90be656186482889039a673d7f243f40549a713
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-25 10:09:51 +0000

    Offer: Added code for making this as entity

--- src/main/java/com/ordering/app/Offer.java
+++ src/main/java/com/ordering/app/Offer.java
@@ -1,4 +1,26 @@
 package com.ordering.app;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class Offer {
+    @Id
+    private Integer id;
+    private String offerId;
+    private String offerName;
+    private String description;
+    @OneToOne
+    private Item itemId;
+    private Float priceReduction;
+    private Integer quantityThreshold;
 }
