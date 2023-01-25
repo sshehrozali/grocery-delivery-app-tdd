fa7aa4b5a04a2a75a0200bd62500a3bb82dd51f7
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-25 09:49:51 +0000

    shouldThrowExceptionIfNoItemsFoundInStock: Created new test method Test failed Test passed Moved test before shouldGetAllItemsWithTheirAvailableQuantity

--- src/test/java/com/ordering/app/CustomerServiceTest.java
+++ src/test/java/com/ordering/app/CustomerServiceTest.java
@@ -1,19 +1,38 @@
 package com.ordering.app;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomerServiceTest {
-    private CustomerService customerService;
+    @InjectMocks
+    private CustomerService serviceUnderTest;
+
+    @Mock
+    private ItemRepository itemRepository;
+    @Mock
+    private LineRepository lineRepository;
 
     @Test
-    @DisplayName("Should Get All Items")
-    void shouldGetAllItems() {
-        assertThat(customerService.getAllItems()).isNotNull();
+    @DisplayName("Should Throw Exception If No Item Found In Stock")
+    void shouldThrowExceptionIfNoItemsFoundInStock() {
+        assertThrows(RuntimeException.class, () -> {
+            serviceUnderTest.getAllItems();
+        });
     }
+
+    @Test
+    @DisplayName("Should Get All Items With Their Available Quantity")
+    void shouldGetAllItemsWithTheirAvailableQuantity() {
+        assertThat(serviceUnderTest.getAllItems()).isNotNull();
+    }
+
 }
