db426da09a9ea9ecd2cf6cf37878d207879542c7
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-25 13:29:47 +0000

    getAllItems: Refactored

--- src/main/java/com/ordering/app/CustomerService.java
+++ src/main/java/com/ordering/app/CustomerService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -12,11 +13,25 @@ public class CustomerService {
     private final LineRepository lineRepository;
     private final OfferRepository offerRepository;
 
-    public List<Line> getAllItems() {
+    public List<LineDTO> getAllItems() {
         if (lineRepository.findAll().isEmpty()) {
             throw new RuntimeException("No Items available in Stock yet. Come back later.");
         }
-        return lineRepository.findAll();
+        return lineRepository.findAll()
+                .stream()
+                .map(
+                        line -> LineDTO.builder()
+                                .itemId(
+                                        ItemDTO.builder()
+                                                .id(line.getItemId().getItemId())
+                                                .name(line.getItemId().getItemName())
+                                                .description(line.getItemId().getDescription())
+                                                .price(line.getItemId().getPrice())
+                                                .build()
+                                )
+                                .quantity(line.getQuantity())
+                                .build()
+                ).collect(Collectors.toList());
     }
 
     public List<Offer> getAllAvailableOffersForEachItems() {
