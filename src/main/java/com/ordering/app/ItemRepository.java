fb7a510cfd4e79843fc904f845e6c20c0ce70875
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-25 09:37:52 +0000

    ItemRepository: Added code for making this repository

--- src/main/java/com/ordering/app/ItemRepository.java
+++ src/main/java/com/ordering/app/ItemRepository.java
@@ -1,4 +1,8 @@
 package com.ordering.app;
 
-public interface ItemRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Integer> {
 }
