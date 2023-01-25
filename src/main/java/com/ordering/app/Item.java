4c4fbd2ef130a64c0628fb8a61dfc7c82416b4fc
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-25 09:29:09 +0000

    Item: Added code for making Item as entity

--- src/main/java/com/ordering/app/Item.java
+++ src/main/java/com/ordering/app/Item.java
@@ -1,2 +1,23 @@
-package com.ordering.app;public class Item {
+package com.ordering.app;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Item {
+    @Id
+    private Integer id;
+    private String itemId;
+    private String itemName;
+    private String price;
 }
