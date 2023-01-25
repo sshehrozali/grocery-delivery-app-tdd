54301d1dea251679540a68fe8532b06aeceb5148
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-25 14:38:54 +0000

    Refactored

--- src/main/java/com/ordering/app/StaffService.java
+++ src/main/java/com/ordering/app/StaffService.java
@@ -3,8 +3,14 @@ package com.ordering.app;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class StaffService {
     private LineRepository lineRepository;
+
+    public List<Line> getAllItemsInInventoryWithQuantity() {
+        return lineRepository.findAll();
+    }
 }
