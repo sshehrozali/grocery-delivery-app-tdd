f1f53de2ac892a22c8daed6c217e16f06f561022
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-30 09:55:24 +0000

    ItemRepositoryTest: Added @DataJpaTest annotation

--- src/test/java/com/ordering/app/repository/ItemRepositoryTest.java
+++ src/test/java/com/ordering/app/repository/ItemRepositoryTest.java
@@ -2,9 +2,11 @@ package com.ordering.app.repository;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class ItemRepositoryTest {
 
     @Test
