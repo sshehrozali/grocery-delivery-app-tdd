f97e05742901086998fddaae85fe8c85e92d4cc4
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-30 09:56:35 +0000

    ItemRepositoryTest: Injected ItemRepository Added @RequiredArgsConstructor for DI

--- src/test/java/com/ordering/app/repository/ItemRepositoryTest.java
+++ src/test/java/com/ordering/app/repository/ItemRepositoryTest.java
@@ -1,13 +1,15 @@
 package com.ordering.app.repository;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @DataJpaTest
+@RequiredArgsConstructor
 class ItemRepositoryTest {
+
+    private ItemRepository itemRepository;
 
     @Test
     @DisplayName("Should Find Item By Item Id")
