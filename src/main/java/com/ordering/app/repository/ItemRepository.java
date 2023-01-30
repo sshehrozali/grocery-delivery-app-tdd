50f04806503fc5cef20746a82549d1cbb669155a
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-30 09:51:14 +0000

    ItemRepository: Created new JPA derived @Query method to find Item by itemId

--- src/main/java/com/ordering/app/repository/ItemRepository.java
+++ src/main/java/com/ordering/app/repository/ItemRepository.java
@@ -2,8 +2,11 @@ package com.ordering.app.repository;
 
 import com.ordering.app.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
+    @Query
+    Item findItemByItemId(String itemId);
 }
