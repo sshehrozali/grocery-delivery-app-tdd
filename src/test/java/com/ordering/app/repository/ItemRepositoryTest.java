7d9861901b654c19026decc3f713870ef5067d46
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-30 09:53:06 +0000

    shouldFindItemByItemId: Refactored name

--- src/test/java/com/ordering/app/repository/ItemRepositoryTest.java
+++ src/test/java/com/ordering/app/repository/ItemRepositoryTest.java
@@ -1,4 +1,14 @@
+package com.ordering.app.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ItemRepositoryTest {
-  
+
+    @Test
+    @DisplayName("Should Find Item By Item Id")
+    void shouldFindItemByItemId() {
+    }
 }
