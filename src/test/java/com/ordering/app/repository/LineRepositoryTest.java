3aca2838962d66de3e7d0468d471a48c1d4b191c
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-31 10:07:23 +0000

    Added @DataJpaTest Autowired LineRepository

--- src/test/java/com/ordering/app/repository/LineRepositoryTest.java
+++ src/test/java/com/ordering/app/repository/LineRepositoryTest.java
@@ -2,10 +2,16 @@ package com.ordering.app.repository;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class LineRepositoryTest {
+
+    @Autowired
+    private LineRepository lineRepository;
 
     @Test
     @DisplayName("Should Find Line By Item Id")
