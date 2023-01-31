1ea4100d320058fc5c33a6a514b04360781d643a
Shehroz Ali <@sshehroza13@gmail.com@>
2023-01-31 10:05:38 +0000

    shouldFindLineByItemId: Refactored name

--- src/test/java/com/ordering/app/repository/LineRepositoryTest.java
+++ src/test/java/com/ordering/app/repository/LineRepositoryTest.java
@@ -1,4 +1,12 @@
+package com.ordering.app.repository;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class LineRepositoryTest {
-  
+
+    @Test
+    void shouldFindLineByItemId() {
+    }
 }
