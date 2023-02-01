ea4e3dc13f11e699eabfe2275d424d4d1b964e33
Shehroz Ali <@sshehroza13@gmail.com@>
2023-02-01 14:29:02 +0000

    Test passed Refactored

--- src/test/java/com/ordering/app/repository/LineRepositoryTest.java
+++ src/test/java/com/ordering/app/repository/LineRepositoryTest.java
@@ -7,12 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class LineRepositoryTest {
@@ -43,6 +38,6 @@ class LineRepositoryTest {
     @Test
     @DisplayName("Should Not Find Line By Item Id")
     void shouldNotFindLineByItemId() {
-        assertThat(lineRepository.findLineByItemId("item3")).isNotNull();
+        assertThat(lineRepository.findLineByItemId("item3")).isNull();
     }
 }
