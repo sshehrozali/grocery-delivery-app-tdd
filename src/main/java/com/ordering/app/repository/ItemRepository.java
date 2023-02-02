ab0e71fa36693adb37d599fc0de42751575df6e1
Shehroz Ali <@sshehroza13@gmail.com@>
2023-02-02 15:48:25 +0000

    deleteAllByItemId: Created new derived JPA query for deleting all items by itemId

--- src/main/java/com/ordering/app/repository/ItemRepository.java
+++ src/main/java/com/ordering/app/repository/ItemRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
     @Query
     Item findItemByItemId(String itemId);
+
+    @Query
+    void deleteAllByItemId(List<String> itemIds);
 }
