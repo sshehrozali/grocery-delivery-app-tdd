d6130d5f3ce4a3753ec9fad143bc5de1cafc47ce
Shehroz Ali <@sshehroza13@gmail.com@>
2023-02-02 15:57:37 +0000

    shouldDeleteAllItemsByItemId: Test failed

--- src/test/java/com/ordering/app/repository/ItemRepositoryTest.java
+++ src/test/java/com/ordering/app/repository/ItemRepositoryTest.java
@@ -1,11 +1,14 @@
 package com.ordering.app.repository;
 
+import com.ordering.app.dto.DeleteItemsDTO;
 import com.ordering.app.entity.Item;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -41,6 +44,27 @@ class ItemRepositoryTest {
     @Test
     @DisplayName("Should Delete All Items By Item Id")
     void shouldDeleteAllItemsByItemId() {
+        List<Item> savedItems = List.of(
+                Item.builder()
+                        .itemId("item1")
+                        .itemName("Cool Item 1")
+                        .description("Amazing Product 1")
+                        .price(12.4f)
+                        .cost(5.6f)
+                        .build(),
+                Item.builder()
+                        .itemId("item2")
+                        .itemName("Awesome Item 2")
+                        .description("Awesome Product 2")
+                        .price(545.4f)
+                        .cost(885.5f)
+                        .build()
+        );
+        itemRepository.saveAll(savedItems);
 
+        String[] itemIds = {"item1", "item2"};
+        itemRepository.deleteAllByItemId(itemIds);
+
+        assertThat(itemRepository.findAll()).isNotNull();
     }
 }
